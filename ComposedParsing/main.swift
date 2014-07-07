0cadbd1a44d17822e1a69368f8adf1849ad85d50
Ulrich Singer <ucf@hq.ucf.local>
2014-07-07 20:33:36 +0200

    - Updated the Parser demo to something more sensible.

--- ComposedParsing/main.swift
+++ ComposedParsing/main.swift
@@ -17,60 +17,53 @@ import Foundation
 
 let arithmetic = Parser(terminals:["+", "-", "*", "/", "%", "Num", "(", ")"])
 
-arithmetic.rule("term", parses:
-    "(" &> "expr" &> ")" !> { $ in $(2) }
- |> "Num")
+arithmetic.rule("expr", parses: "sum")
+
+arithmetic.rule("sum", parses: "mul" &> "sumTail")
+
+arithmetic.rule("sumTail", parses:
+    "+" &> "mul"
+        &> { $ in ($(0) as NSNumber).doubleValue + ($(2) as NSNumber).doubleValue }
+        &> "sumTail"
+
+ |> "-" &> "mul"
+        &> { $ in ($(0) as NSNumber).doubleValue - ($(2) as NSNumber).doubleValue }
+        &> "sumTail"
+
+ |> { $ in $(0) })
+
+arithmetic.rule("mul", parses: "sgn" &> "mulTail")
+
+arithmetic.rule("mulTail", parses:
+    "*" &> "sgn"
+        &> { $ in ($(0) as NSNumber).doubleValue * ($(2) as NSNumber).doubleValue }
+        &> "mulTail"
+
+ |> "/" &> "sgn"
+        &> { $ in ($(0) as NSNumber).doubleValue / ($(2) as NSNumber).doubleValue }
+        &> "mulTail"
+
+ |> "%" &> "sgn"
+        &> { $ in ($(0) as NSNumber).doubleValue % ($(2) as NSNumber).doubleValue }
+        &> "mulTail"
+
+ |> { $ in $(0) })
 
 arithmetic.rule("sgn", parses:
-    "-" &> "sgn" !> { $ in
-        let rhs = $(2) as NSNumber
-        return -rhs.doubleValue
-    }
- |> "+" &> "sgn" !> { $ in
-        let rhs = $(2) as NSNumber
-        return rhs.doubleValue
-    }
+    "-" &> "sgn" &> { $ in -($(2) as NSNumber).doubleValue }
+ |> "+" &> "sgn" &> { $ in +($(2) as NSNumber).doubleValue }
  |> "term")
 
-arithmetic.rule("mul", parses:
-    "sgn" &> "*" &> "mul" !> { $ in
-        let lhs = $(1) as NSNumber
-        let rhs = $(3) as NSNumber
-        return lhs.doubleValue * rhs.doubleValue
-    }
- |> "sgn" &> "/" &> "mul" !> { $ in
-        let lhs = $(1) as NSNumber
-        let rhs = $(3) as NSNumber
-        return lhs.doubleValue / rhs.doubleValue
-    }
- |> "sgn" &> "%" &> "mul" !> { $ in
-        let lhs = $(1) as NSNumber
-        let rhs = $(3) as NSNumber
-        return lhs.doubleValue % rhs.doubleValue
-    }
- |> "sgn")
-
-arithmetic.rule("sum", parses:
-    "mul" &> "+" &> "sum" !> { $ in
-        let lhs = $(1) as NSNumber
-        let rhs = $(3) as NSNumber
-        return lhs.doubleValue + rhs.doubleValue
-    }
- |> "mul" &> "-" &> "sum" !> { $ in
-        let lhs = $(1) as NSNumber
-        let rhs = $(3) as NSNumber
-        return lhs.doubleValue - rhs.doubleValue
-    }
- |> "mul")
-
-arithmetic.rule("expr", parses: "sum")
+arithmetic.rule("term", parses:
+    "(" &> "expr" &> ")" &> { $ in $(2) }
+ |> "Num")
 
 let tokenSource = ArrayLexer(tokens:[("(",""), ("Num",1), ("-",""), ("Num",6), (")",""), ("/",""), ("+",""), ("Num",4)])
 
 var rule = "expr"<!
 var val: Parser.Result
 let t0 = NSDate()
-for _ in 1..100
+for _ in 1...1000
 {
     val = arithmetic.start(rule, tokenSource)
     tokenSource.reset()
